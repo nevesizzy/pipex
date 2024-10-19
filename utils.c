3a81c0702d9fda5353ff8261fa0ec18265931e97
Isabella <126694876+nevesizzy@users.noreply.github.com>
2024-10-20 00:58:35 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -36,35 +36,6 @@ void	ft_free(char **str)
 	free(str);
 }
 
-char	*get_path(char *cmd, char **envp)
-{
-	char	**envp_paths;
-	char	*cmd_path;
-	int		i;
-	char	*only_path;
-
-	i = 0;
-	while (!ft_strnstr(envp[i], "PATH=", 5))
-		i++;
-	envp_paths = ft_split(envp[i] + 5, ':');
-	i = -1;
-	while (envp_paths[++i])
-	{
-		only_path = ft_strjoin(envp_paths[i], "/");
-		cmd_path = ft_strjoin(only_path, cmd);
-		free(only_path);
-		if (access(cmd_path, F_OK | X_OK) == 0)
-		{
-			ft_free(envp_paths);
-			return (cmd_path);
-		}
-		free(cmd_path);
-	}
-	ft_free(envp_paths);
-	custom_error(cmd, "command not found");
-	return (NULL);
-}
-
 void	run_cmd(char *cmd, char **envp)
 {
 	char	**cmd_and_flags;
